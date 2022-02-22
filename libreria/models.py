c1ab8bf3ac3c2dfdc0d44c27178e38cf6a2195b8
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-22 19:59:17 -0400

    creando el modelo de datos para la bd con todos sus respectivos campos

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to= 'imagenes/',verbose_name='Imagen', null=True)
+    description = models.TextField( verbose_name= 'Descripcion',null=True)
