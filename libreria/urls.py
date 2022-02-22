2dc23a40bc74b23dc540bc4c6de113c65df7231b
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-22 17:02:44 -0400

    agregando funcionalidad al button del index y que me redirija a otro formulario

--- libreria/urls.py
+++ libreria/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.inicio,name='inicio'), #esta es la ruta de la app libreria
     path('nosotros', views.nosotros, name='nosotros'),
     path('libros',views.libros,name='libros'),
+    path('libros/crear',views.crear,name='crear')
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -3,10 +3,13 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'paginas.html')
+    return render(request, 'paginas/inicio.html')
 
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 def libros(request):
     return render(request, 'libros/index.html')
+
+def crear(request): # esta funcion es para crear un libro
+    return render(request, 'libros/crear.html')
