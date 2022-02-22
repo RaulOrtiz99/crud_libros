3dbaaa3b7cc4b12bcf9d05da2b6bbd4495a7b6e3
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-22 19:28:39 -0400

    cambiando las configuraciones para conectarme a una bd mysql

--- crud_libros/settings.py
+++ crud_libros/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'crud_libros.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'libreria',
+        'USER':'root',
+        'PASSWORD':'namaste1234',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
