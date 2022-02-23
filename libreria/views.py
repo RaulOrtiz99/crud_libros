88e6b8f34c8b8a55ed280bbc9a507fa090d7721b
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-23 19:00:57 -0400

    agregando el guardado de libros, es decir ya se guaradan datos y funciona el formulario para guardar

--- libreria/views.py
+++ libreria/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
+
 
 # Create your views here.
 
@@ -20,8 +21,11 @@ def libros(request):  # con esta funcion devolvemos la lista de todos los libros
 
 
 def crear(request):  # esta funcion es para crear un libro
-    formulario = LibroForm(request.POST or None)
-    return render(request, 'libros/crear.html',{'formulario':formulario})
+    formulario = LibroForm(request.POST or None, request.FILES or None)  # acá se obtienen los datos
+    if formulario.is_valid():  # acá validamos los datos del formulario
+        formulario.save()  # aca se guardan
+        return redirect('libros')  # y se redirecciona a libros
+    return render(request, 'libros/crear.html', {'formulario': formulario})
 
 
 def editar(request):
