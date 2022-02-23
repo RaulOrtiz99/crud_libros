061c84b33a36213c86b63836c73d10c624f71163
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-22 20:03:19 -0400

    registrando el modelo en el panel administrativo

--- libreria/models.py
+++ libreria/models.py
@@ -6,4 +6,4 @@ class Libro(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name='Titulo')
     imagen = models.ImageField(upload_to= 'imagenes/',verbose_name='Imagen', null=True)
-    description = models.TextField( verbose_name= 'Descripcion',null=True)
+    descripcion = models.TextField( verbose_name= 'Descripcion',null=True)
